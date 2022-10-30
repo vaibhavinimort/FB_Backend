b59e1129e80bb9a92aaa0a9798c68440dc1e0eef
vaibhavinimrot@gmail.com <113259829+vaibhavinimort@users.noreply.github.com>
2022-10-30 11:43:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
+    console.log("Testing")
     res.send("welcome from ashish");
 });
 app.get("/books", (req, res) => {
+    console.log("Testing 1")
     res.send("welcome from ashis home");
 });
 app.listen(8000, () => {
+    console.log("Testing 2")
     console.log(`Running on hello 8000`);
 });
