bfdcbfb76a4f62bf6ae10ba39e67ba2c65ab7b26
vaibhavinimrot@gmail.com <113259829+vaibhavinimort@users.noreply.github.com>
2022-11-27 22:50:13 +0530

    added generateToken throough payload using jwt npm

--- helpers/token.js
+++ helpers/token.js
@@ -1,8 +1,8 @@
 const jwt = require("jsonwebtoken");
 
 
-exports.generateToken = (payload, expired = 30) => {
+exports.generateToken = (payload, expired) => {
     return jwt.sign(payload, process.env.TOKEN_SECRETE, {
-        expiresIn: expired
+        expiresIn: expired,
     });
 };
