5d997b2b56cc972470763ab3386f4e8dab75835b
vaibhavinimrot@gmail.com <113259829+vaibhavinimort@users.noreply.github.com>
2022-12-03 16:26:05 +0530

    login api with token generate with valid credential

--- controllers/user.js
+++ controllers/user.js
@@ -115,10 +115,26 @@ exports.login = async(req, res) => {
     try {
         const { email, password } = req.body;
         console.log({ email });
-        const user = User.findOne({ email });
+        const user = await User.findOne({ email });
         if (!user) {
             return res.status(400).json({ message: "the email addres you entered is not connected to an account" });
         }
+        const checkPasssword = await bcrypt.compare(password, user.password);
+        if (!checkPasssword) {
+            return res.status(400).json({ message: "invalid credential. Please try later." });
+
+        }
+        const token = generateToken({ id: user._id.toString() }, "7d")
+        res.send({
+            id: user._id,
+            username: user.username,
+            picture: user.picture,
+            first_name: user.first_name,
+            last_name: user.last_name,
+            token: token,
+            verified: user.verified,
+            message: 'register success ! please activate your email to start',
+        });
     } catch (error) {
         return res.status(500).json({ message: error.message })
 
