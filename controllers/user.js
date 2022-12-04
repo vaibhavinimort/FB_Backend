2064afd61a96361433deadf67178039bbb9c8711
vaibhavinimrot@gmail.com <113259829+vaibhavinimort@users.noreply.github.com>
2022-12-04 22:03:03 +0530

    addede sendcerification api and check valiUser

--- controllers/user.js
+++ controllers/user.js
@@ -91,13 +91,17 @@ exports.register = async(req, res) => {
 };
 
 exports.activateAccount = async(req, res) => {
-    const { token } = req.params;
     try {
+        const { token } = req.params;
+        const validUser = req.user.id;
         const user = jwt.verify(token, process.env.TOKEN_SECRETE);
         console.log(user);
 
         const check = await User.findById(user.id);
         console.log(check);
+        if (validUser !== user.id) {
+            return res.status(400).json({ message: "you dont have the authorization to complete this operation" });
+        }
 
         if (check.verified == true) {
             return res.status(400).json({ message: "this email is already verifies" })
@@ -140,4 +144,23 @@ exports.login = async(req, res) => {
 
     }
 
+}
+
+exports.sendVerification = async(req, res) => {
+    try {
+        const id = req.user.id;
+        const user = await User.findById(id);
+        if (user.verified === true) {
+            return res.status(400).json({ message: "this account is already activated" });
+        }
+        const emailVerificationToken = generateToken({ id: user._id.toString() },
+            "30m"
+        );
+        const url = `${process.env.BASE_URL}/activate/${emailVerificationToken}`
+        sendVerificationEmail(user.email, user.first_name, url);
+        return res.status(200).json({ message: "Email verification link has been send to your email" });
+
+    } catch (error) {
+        res.status(500).json({ message: "message.error" })
+    }
 }
