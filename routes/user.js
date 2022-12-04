ed95d0cdcd31014d5ce913156b7a62b41b5fd30e
vaibhavinimrot@gmail.com <113259829+vaibhavinimort@users.noreply.github.com>
2022-12-04 22:04:20 +0530

    addded routes activateAccount, login, auth, sendverification

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,14 @@
 const express = require("express");
-const { register, activateAccount, login } = require("../controllers/user");
+const { register, activateAccount, login, auth, sendVerification } = require("../controllers/user");
 const router = express.Router();
+const { authUser } = require("../middleware/auth.js");
 
 router.post("/register", register);
-router.get("/activate/:token", activateAccount);
+router.get("/activate/:token", authUser, activateAccount);
 router.post("/login", login);
-
+router.post("/login", login);
+router.post("/sendVerification", authUser, sendVerification);
+router.post("/auth", authUser, auth);
 
 
 
