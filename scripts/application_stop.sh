8a3fe4461d94e2cce0c89c030fcbe3981e2c973b
vaibhavinimrot@gmail.com <113259829+vaibhavinimort@users.noreply.github.com>
2022-12-06 23:57:52 +0530

    Update application_stop.sh

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 #Stopping existing node servers
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install node
+nvm install 16
+nvm use 16
+npm install pm2@latest -g
+
 echo "Stopping any existing node servers"
-pkill node
 
 pm2 stop FBServer  || :
 pm2 delete FBServer  || :
+pkill node
