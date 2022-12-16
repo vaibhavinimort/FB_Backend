a4f77d796a50c78d9fa896a4f589daf1f3b64d70
vaibhavinimrot@gmail.com <113259829+vaibhavinimort@users.noreply.github.com>
2022-12-16 22:46:56 +0530

    create post api in controller

---
+++ controllers/post.js
@@ -0,0 +1,11 @@
+const { post } = require("../routes/post")
+
+exports.createPost = async(req, res) {
+    try {
+        const post = await new post(teq.body).save();
+        res.json(post);
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+
+}
