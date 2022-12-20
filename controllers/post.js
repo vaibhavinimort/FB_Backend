ccd3319f59a6e5e4ae8f01a86de397dc63df8383
vaibhavinimrot@gmail.com <113259829+vaibhavinimort@users.noreply.github.com>
2022-12-21 00:02:37 +0530

    added createpost api

--- controllers/post.js
+++ controllers/post.js
@@ -1,6 +1,6 @@
 const { post } = require("../routes/post")
 
-exports.createPost = async(req, res) {
+exports.createPost = async(req, res) => {
     try {
         const post = await new post(teq.body).save();
         res.json(post);
