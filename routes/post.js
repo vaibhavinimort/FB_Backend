61bfc64a02dda546b26178f3f666ff47b131e8d7
vaibhavinimrot@gmail.com <113259829+vaibhavinimort@users.noreply.github.com>
2023-01-13 05:21:02 +0530

    Update post.js

--- routes/post.js
+++ routes/post.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const { createPost } = require("../controllers/post");
+const { createPost, getAllPosts } = require("../controllers/post");
 const router = express.Router();
 const { authUser } = require("../middleware/auth.js");
 
 router.post("/createPost", authUser, createPost);
+router.get("/getAllPosts", getAllPosts);
+
 
 
 module.exports = router;
