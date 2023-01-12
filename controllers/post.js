a3248af6fdb3ba99214525d7084140dcae1363fc
vaibhavinimrot@gmail.com <113259829+vaibhavinimort@users.noreply.github.com>
2023-01-13 05:18:16 +0530

    get all post api

--- controllers/post.js
+++ controllers/post.js
@@ -1,11 +1,19 @@
-const { post } = require("../routes/post")
+const Post = require("../models/Post")
 
 exports.createPost = async(req, res) => {
     try {
-        const post = await new post(teq.body).save();
+        const post = await new Post(req.body).save();
         res.json(post);
     } catch (error) {
         res.status(500).json({ message: error.message })
     }
+}
 
+exports.getAllPosts = async(req, res) => {
+    try {
+        const posts = await Post.find();
+        res.json(posts);
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
 }
