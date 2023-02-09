9556d727ef3481763ec1e51ad9c90565a6ccd5ad
vaibhavinimrot@gmail.com <113259829+vaibhavinimort@users.noreply.github.com>
2023-02-09 18:37:08 +0530

    added paths createpost deletepost comment savepost getallpost

--- routes/post.js
+++ routes/post.js
@@ -1,11 +1,21 @@
 const express = require("express");
-const { createPost, getAllPosts } = require("../controllers/post");
-const router = express.Router();
+const {
+    createPost,
+    getAllPosts,
+    comment,
+    savePost,
+    deletePost
+} = require("../controllers/post");
+
 const { authUser } = require("../middleware/auth.js");
 
-router.post("/createPost", authUser, createPost);
-router.get("/getAllPosts", getAllPosts);
+const router = express.Router();
 
+router.post("/createPost", authUser, createPost);
+router.get("/getAllPosts", authUser, getAllPosts);
+router.put("/comment", authUser, comment);
+router.put("/savePost/:id", authUser, savePost);
+router.delete("/deletePost/:id", authUser, deletePost);
 
 
 module.exports = router;
